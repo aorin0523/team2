c017265d7304c058c2aa18b4d99661fbf14de93f
UchimaS <amamya0809love@gmail.com>
2025-06-16 16:51:45 +0900

    add: kigyohomeを追加

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import KigyoHome from "./pages/KigyoHome";
 
 function App() {
-  return (
+  return ( 
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/enterprise" element={<KigyoHome />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/frontend/src/pages/KigyoHome.jsx
@@ -0,0 +1,118 @@
+import { useState } from 'react'
+import flow2 from '../images/flow_02.png'
+import flow3 from '../images/flow_03.png'
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+import '../css/kigyohome.css'
+
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const drawerWidth = 248;
+
+  return (
+    <>
+  <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Permanent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        <Toolbar />
+
+      </Box>
+    </Box>
+
+
+
+    <a href="#" class="button">
+    <img src={flow3}/>
+        <span>募集フォーム 新規作成</span>
+    </a>
+
+    <a href="#" class="button">
+        
+        <img src={flow2}/>
+        <span>オファーページ</span>
+    </a>
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
+
