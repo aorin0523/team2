043d02a2498d929d9a69c940760168e3335e26e5
Aoisgym <136062993+Aoisgym@users.noreply.github.com>
2025-06-17 09:46:21 +0900

    add: ユーザー側案件詳細追加

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
+import UCD from "./pages/UserCaseDetails";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/user/details" element={<UCD />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/frontend/src/pages/UserCaseDetails.jsx
@@ -0,0 +1,75 @@
+import Icon from '@mui/material/Icon';
+import '../App.css'
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MailIcon from '@mui/icons-material/Mail';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import IconButton from '@mui/material/IconButton';
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+function App() {
+
+return (
+    <>
+    <AppBar position="fixed"> {/* ヘッダーをページ上部に固定する場合 */}
+      <Toolbar>
+        {/*
+          Typographyコンポーネントは、ロゴやサイトタイトルとして使用できます。
+          variant="h6" は適切な見出しレベルのスタイルを適用します。
+          noWrap はテキストが長すぎても折り返さないようにします。
+          component="div" は、このTypographyがHTMLのdivとしてレンダリングされることを意味します。
+          もしReact RouterのLinkとして使いたい場合は、component={Link} to="/" のように変更します。
+        */}
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          <div className='header-left'>
+            <a href='/'>
+                <h6>ぱいざ</h6>
+            </a>
+        </div>
+        </Typography>
+
+        {<div className='header-right'>
+            <ul>
+                <li><a href='/'>仕事を探す</a></li>
+                <li><a href='/'>仕事の内容</a></li>
+                <li>
+                    <IconButton sx={{ color : "white"}} size="small" >
+                        <BookmarkIcon />
+                    </IconButton>
+                </li>
+                <li>
+                    <IconButton sx={{ color : "white"}} size="small" >
+                        <NotificationsIcon />
+                    </IconButton>
+                </li>
+                <li>
+                    <IconButton sx={{ color : "white"}} size="small" >
+                        <MailIcon />
+                    </IconButton>
+                </li>
+                <li>
+                    <IconButton sx={{ color : "white"}} size="small" >
+                        <HelpOutlineIcon />
+                    </IconButton>
+                </li>
+                <li>
+                    <IconButton sx={{ color : "white"}} size="small" >
+                        <AccountCircleIcon />
+                    </IconButton>
+                </li>
+            </ul>
+        </div>}
+      </Toolbar>
+    </AppBar>
+      <body>
+
+      </body>
+    </>
+  )
+}
+
+export default App
