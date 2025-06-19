fd4dc8356332dbaacffb4656e35fe7ce87c6def2
takayamayoshihisa1013 <takayama.yoshihisa1013@gmail.com>
2025-06-19 13:23:48 +0900

    add: usersの処理の追加

--- src/backend/modules/db/users.py
+++ src/backend/modules/db/users.py
@@ -1,4 +1,5 @@
 from modules.db import BaseDB
+import uuid
 
 class Users(BaseDB): 
     def __init__(self):
@@ -12,3 +13,71 @@ class Users(BaseDB):
             query = self.users.select()
             result = conn.execute(query)
             return [dict(row) for row in result.mappings()]
+        
+    def get_users(self):
+        """
+        ユーザ情報を全件取得する
+        """
+        with self.engine.connect() as conn:
+            query = self.users.select()
+            result = conn.execute(query)
+            return [dict(row) for row in result.mappings()]
+        
+    def create_user(self, name, email, password):
+        """
+        ユーザーを追加する
+        """
+        id = uuid.uuid4()
+        try:
+            with self.engine.connect() as conn:
+                query = self.users.insert().values(
+                    id=id,
+                    name=name,
+                    email=email,
+                    password=password
+                )
+                conn.execute(query)
+                conn.commit()
+                return {"user_id":id, "status": "ok"}
+        except Exception as e:
+            return {"status": "ng", "error": e}
+    
+    def update_user(self, id, name=None, email=None, password=None):
+        """
+        ユーザー情報をアップデート
+        """
+        try:
+            with self.engine.connect() as conn:
+                update_values = {}
+                if name is not None:
+                    update_values["name"] = name
+                if email is not None:
+                    update_values["email"] = email
+                if password is not None:
+                    update_values["password"] = password
+                
+                query = self.users.update().where(
+                    self.users.c.id==id
+                ).values(**update_values)
+                
+                conn.execute(query)
+                conn.commit()
+                return {"status": "ok"}
+        except Exception as e:
+            return {"status": "ng", "error": e}
+
+    def delete_user(self, id):
+        """
+        ユーザー情報を削除
+        """
+        try:
+            with self.engine.connect() as conn:
+                query = self.users.delete().where(
+                    self.users.c.id==id
+                )
+                
+                conn.execute(query)
+                conn.commit()
+                return {"status": "ok"}
+        except Exception as e:
+            return {"status": "ng", "error": e}
---
+++ src/backend/users/user.py
@@ -0,0 +1,86 @@
+from fastapi import APIRouter, Request, FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+import io
+import uuid
+
+router = APIRouter()
+
+@router.get("/")
+def get(request: Request): 
+    """
+    見本用GETエンドポイント
+    """
+    return {"message": "Test endpoint is working!"}
+
+from pydantic import BaseModel
+class TestData(BaseModel):
+    name: str
+    value: int
+
+@router.post("/")
+async def post(request: Request, data: TestData):
+    """
+    見本用POSTエンドポイント
+    """
+    return {"message": "Test endpoint updated data!", "data": data}
+
+@router.put("/")
+async def put(request: Request, data: TestData):
+    """
+    見本用PUTエンドポイント
+    """
+    return {"message": "Test endpoint updated data!", "data": data}
+
+@router.delete("/")
+async def delete(request: Request, data: TestData):
+    """
+    見本用DELETEエンドポイント
+    """
+    return {"message": "Test endpoint updated data!", "data": data}
+
+# MinIOにファイルをアップロードするときの見本
+from modules.minio import upload_file, download_file
+
+@router.post("/upload")
+async def upload(request: Request, file: UploadFile = File(...)):
+    """
+    ファイルアップロードの見本用エンドポイント
+    """
+    
+    content = await file.read()
+    file_uuid = str(uuid.uuid4())
+
+    upload_file(
+        bucket_name="temp",
+        file_name=file_uuid,
+        file_content_type=file.content_type,
+        file_content=content
+    )
+    return {"status": "ok", "file_name": file.filename, "file_uuid": file_uuid}
+
+@router.get("/download/{file_name}")
+async def download(file_name: str):
+    """
+    ファイルダウンロードの見本用エンドポイント
+    """
+
+    result = download_file(file_name, bucket_name="temp")
+    if result["status"] == "ok":
+        return StreamingResponse(
+            io.BytesIO(result["content"]),
+            media_type=result["content_type"],
+            headers={"Content-Disposition": f"attachment; filename={file_name}"}
+        )
+    else:
+        return None, 404, {"message": result["message"]}
+
+@router.get("/users")
+async def get_users():
+    """
+    ユーザ情報を全件取得するエンドポイント
+    """
+
+    from modules.db.users import Users
+    
+    users = Users()
+    return users.get_all_users()
