32b15e8bd2b7ca5767f740dba85bdab6c1251396
smile0419-cyber <egaoqiao66@gmail.com>
2025-06-23 16:54:25 +0900

    add:ルーティング追加とadd:Jobs.cssファイル追加

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
       </Routes>
     </BrowserRouter>
   );
