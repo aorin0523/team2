6964eb5c482e234607a060fe1e42b457137e1341
UchimaS <amamya0809love@gmail.com>
2025-06-30 14:44:48 +0900

    update: Offerlistページの追加

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Offerlist from "./pages/Offerlist";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Offerlist" element={<Offerlist />} />
       </Routes>
     </BrowserRouter>
   );
