ad1e9506ef146bb71b8ff33a7ef09c9ed9efb42c
UchimaS <amamya0809love@gmail.com>
2025-06-30 15:24:51 +0900

    update: 各名前の変更

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Offerlist from "./pages/Offerlist";
+import Offerdetail from "./pages/Offerdetail";
 
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Offerlist" element={<Offerlist />} />
+        <Route path="/Offerdetail" element={<Offerdetail />} />
       </Routes>
     </BrowserRouter>
   );
