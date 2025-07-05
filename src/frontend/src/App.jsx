f50b0105cef4ab9aa0b77f3f1721ed7888317119
suzuki <lling4628@gmail.com>
2025-07-06 02:05:05 +0900

    add: headerとsideの統一化

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Header from "./components/Header";
+import Side from "./components/Side";
 import Home from "./pages/Home";
 import Offer from "./pages/Offer";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/offer" element={<Offer />} />
-      </Routes>
+      <Box sx={{ height: '100vh', width: '100vw' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offer" element={<Offer />} />
+          <Route path="/header" element={<Header />} />
+          <Route path="/side" element={<Side />} />
+        </Routes>
+      </Box>
     </BrowserRouter>
   );
 }
