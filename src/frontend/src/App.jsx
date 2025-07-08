9ed2f8285f6d4e695e8c018670b2aeb05ec0c07f
suzuki <lling4628@gmail.com>
2025-07-08 13:32:42 +0900

    add: 企業側プロフィールの作成

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -10,18 +10,20 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import KigyoHome from "./pages/KigyoHome";
 import Offer from "./pages/Offer";
+import Profile from "./pages/Profile";
 import "./App.css";
 
 function App() {
-  return ( 
+  return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-    　　　<Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/enterprise" element={<KigyoHome />} />
           <Route path="/offer" element={<Offer />} />
+          <Route path="/Profile" element={<Profile />} />
           <Route path="/header" element={<Header />} />
           <Route path="/side" element={<Side />} />
         </Routes>
