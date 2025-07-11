516eb91eb478b6bc11d1bb82c657b6a6e6b974ba
lea-151107 <lea.rate151107@gmail.com>
2025-07-11 16:35:23 +0900

    update: ルーター変更

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import Header from "./components/Header";
 import Side from "./components/Side";
 import { AuthProvider } from "./contexts/AuthContext";
@@ -14,27 +14,44 @@ import KigyoHome from "./pages/KigyoHome";
 import Offer from "./pages/Offer";
 import Profile from "./pages/Profile";
 import List from "./pages/Project_List";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/enterprise" element={<KigyoHome />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/offer" element={<Offer />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/header" element={<Header />} />
-          <Route path="/side" element={<Side />} />
-          <Route path="/Offerdetail" element={<Offerdetail />} />
-          <Route path="/user/details" element={<UCD />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+
+              <Route index element={<Home />} />
+              <Route path="signin" element={<SignIn />} />
+              <Route path="signup" element={<SignUp />} />
+
+            <Route path="/user/*" element={<Header sx={{ margin: 0 }} />}>
+              <Route path="list" element={<List />} />
+              <Route path="offer" element={<Offer />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="side" element={<Side />} />
+              <Route path="offerdetail" element={<Offerdetail />} />
+              <Route path="details" element={<UCD />} />
+            </Route>
+          </Routes>
+
+          {/* <Routes
+            path="kigyo/*"
+            element={
+              <Stack direction={"row"}>
+                <Box>
+                  <Side />
+                </Box>
+              </Stack>
+            }
+          >
+            <Route path="/" element={<KigyoHome />} />
+          </Routes> */}
+        </BrowserRouter>
+      </AuthProvider>
+    </>
   );
 }
 
